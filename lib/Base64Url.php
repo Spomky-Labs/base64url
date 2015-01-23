b36f4bbd3d24187e26d9dda5e9d44a9fbf013984
Spomky <florent+git@morselli.fr>
2015-01-23 13:51:27 +0100

    PHPDoc

--- lib/Base64Url.php
+++ lib/Base64Url.php
@@ -7,6 +7,11 @@ namespace Base64Url;
  */
 class Base64Url
 {
+    /**
+     * @param  string  $data        The data to encode
+     * @param  boolean $use_padding If true, the "=" padding at end of the encoded value are kept, else it is removed
+     * @return string  The data encoded
+     */
     public static function encode($data, $use_padding = false)
     {
         $encoded = strtr(base64_encode($data), '+/', '-_');
@@ -14,6 +19,10 @@ class Base64Url
         return true === $use_padding ? $encoded : rtrim($encoded, '=');
     }
 
+    /**
+     * @param  string $data The data to decode
+     * @return string The data decoded
+     */
     public static function decode($data)
     {
         return base64_decode(strtr($data, '-_', '+/'));
