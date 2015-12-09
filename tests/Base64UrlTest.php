00f5597e47f56945248e4c1e8538a9fe78b71ce2
Florent Morselli <florent+git@morselli.fr>
2015-12-09 02:57:34 -0500

    Applied fixes from StyleCI

--- tests/Base64UrlTest.php
+++ tests/Base64UrlTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2015 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 use Base64Url\Base64Url;
 
 class Base64UrlTest extends \PHPUnit_Framework_TestCase
@@ -66,6 +75,7 @@ class Base64UrlTest extends \PHPUnit_Framework_TestCase
             ],
         ];
     }
+
     /**
      * @dataProvider getTestBadVectors
      */
